4b66cb352fd7c02aeeeec5052fe58160b1a5a452
santi <santiagodelafuente2013@gmail.com>
2022-03-23 15:41:08 -0300

    performance

--- exercise_five/exercise_five.rb
+++ exercise_five/exercise_five.rb
@@ -1,15 +1,6 @@
 # frozen_string_literal: true
 
-def suma_cuadrados
-  sum_total = 0
-  (1..100).each do |iterator|
-    sum_total += iterator**2
-  end
-  sum_total
-end
-
-def cuadrados_suma
-  (1..100).sum**2
-end
+suma_cuadrados = (1..100).map { |iterator| iterator**2 }.inject(:+)
+cuadrados_suma = (1..100).inject(:+)**2
 
 puts "La diferencia entre el cuadrado de la suma y la suma de los cuadrados de los primeros 100 números naturales es = #{cuadrados_suma - suma_cuadrados}"
