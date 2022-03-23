b5ed3d53fa2a920d597d2885f001cb7bab58bf6c
santi <santiagodelafuente2013@gmail.com>
2022-03-23 15:46:26 -0300

    performance

--- exercise_eight/exercise_eight.rb
+++ exercise_eight/exercise_eight.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 
 def length_string_in_array(string)
-  new_array = []
-  string.map do |iterator|
-    count = 0
-    iterator.chars.each { count += 1 }
-    new_array << count
-  end
-
-  new_array
+  string.map(&:size)
 end
 
-p "Resultado de [‘Ruby’, ‘is’, ‘awesome’] es = #{length_string_in_array %w[Ruby is awesome]}"
+puts "Resultado de [‘Ruby’, ‘is’, ‘awesome’] es = #{length_string_in_array %w[Ruby is awesome]}"
