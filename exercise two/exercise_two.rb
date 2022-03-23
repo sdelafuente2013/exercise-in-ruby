a74a516dee74dab8d6851de5aecd4cbe782e457c
santi <santiagodelafuente2013@gmail.com>
2022-03-23 12:00:11 -0300

    performance

--- exercise two/exercise_two.rb
+++ exercise two/exercise_two.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-def fibonacci
+def fibonacci(max)
   fib = [0, 1]
-  (2..4_000_000).each do |index|
-    if fib[index - 1] + fib[index - 2] < 4_000_000
+  (2..max).each do |index|
+    if fib[index - 1] + fib[index - 2] < max
       fib << fib[index - 2] + fib[index - 1]
     else
       return fib
@@ -12,7 +12,7 @@ def fibonacci
 end
 
 def sum_pares(array)
-  array.select { |number| number if number.even? }.sum
+  array.select { |number| number if number.even? }.inject(:+)
 end
 
-puts "Suma de la secuencia de Fibonacci que no exceden los 4 millones y son pares es = #{sum_pares(fibonacci)}"
+puts "Suma de la secuencia de Fibonacci que no exceden los 4 millones y son pares es = #{sum_pares(fibonacci(4_000_000))}"
