1257c1820c22f558221e875d232645341371e682
santi <santiagodelafuente2013@gmail.com>
2022-03-23 11:51:18 -0300

    performance

--- exercise one/exercise_one.rb
+++ exercise one/exercise_one.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-def multiplos_tres_cinco(limit)
-  array_rang = 1...limit
-  array_rang.select { |number| number if (number % 3).zero? || (number % 5).zero? }.sum
+def multiplos_tres_cinco(max)
+  (1...max).select { |number| number if (number % 3).zero? || (number % 5).zero? }.inject(:+)
 end
 
 puts "La suma de todos los múltiplos de 3 o 5 menores que 1000 es #{multiplos_tres_cinco(1000)}"
